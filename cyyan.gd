558eeaf456a5d4f7168ac6a403e17941da695ecb
Beeka <beekayt274@gmail.com>
2023-07-31 21:33:07 +1200

    balancing (please playtest afterwards) -cyyancosted +cyyanalltime magenter is now in information

--- cyyan.gd
+++ cyyan.gd
@@ -8,7 +8,7 @@ func _ready():
 
 var thingspercyyan = float(1000000)
 var cyyanthings = float(0)
-var cyyanthingscosted = float(0)
+var cyyanthingsalltime = float(0)
 var cyyanmultiply = float(1)
 
 const PASSIVENOCOSTCOST = float(100)
@@ -31,39 +31,40 @@ const MAGENTERMECHANICCOST = float(1e12)
 var magentermechanic = false
 
 
-func processcyyan(_delta):
+func processcyyan(delta):
+	cyyanthings += %normal.persecondactual*delta / thingspercyyan
+	cyyanthingsalltime += %normal.persecondactual*delta / thingspercyyan
+	
 	if !%normal.cyyanmechanic: return
-	cyyanthings = floor(%normal.thingsalltime / thingspercyyan) - cyyanthingscosted
-	if pow(cyyanthings, 0.7) + 1 < 1e6:
-		cyyanmultiply = pow(cyyanthings, 0.7) + 1
+	if pow(floor(cyyanthings), 0.7) + 1 < 1e6:
+		cyyanmultiply = pow(floor(cyyanthings), 0.7) + 1
 	else:
-		cyyanmultiply = pow(log(cyyanthings-4e8), 5.1)/3 + 1e6
+		cyyanmultiply = pow(cyyanthings, 0.5) + 1e6
 	@warning_ignore("integer_division")
 	timeplayedboost = clampf(game.time/500, 1, 500)
 
 func _passivenocost():
-	cyyanthingscosted += PASSIVENOCOSTCOST
+	cyyanthings -= PASSIVENOCOSTCOST
 	passivenocost = true
 	%passivenocost.text = "passive thing generators no\nlonger costs things"
 	_update_passivenocost()
 	achiev.achs[15].unlock()
 
 func _gainclick():
-	cyyanthingscosted += GAINCLICKCOST
+	cyyanthings -= GAINCLICKCOST
 	gainclick = true
-	_update_gainclick()
 
 func _automatedpassive():
-	cyyanthingscosted += AUTOMATEDPASSIVECOST
+	cyyanthings -= AUTOMATEDPASSIVECOST
 	automatedpassive = true
 	_update_automatedpassive()
 
 func _timeplayed():
-	cyyanthingscosted += TIMEPLAYEDCOST
+	cyyanthings -= TIMEPLAYEDCOST
 	timeplayed = true
 
 func _achievsquared():
-	cyyanthingscosted += ACHIEVSQUAREDCOST
+	cyyanthings -= ACHIEVSQUAREDCOST
 	achievsquared = true
 	achiev._update_ach()
 	_update_achievsquared()
@@ -72,7 +73,7 @@ func _achievsquared():
 func _magentermechanic():
 	_update_achievsquared()
 	magentermechanic = true
-	cyyanthingscosted += MAGENTERMECHANICCOST
+	cyyanthings -= MAGENTERMECHANICCOST
 	_update_magentermechanic()
 	achiev.achs[17].unlock()
 
@@ -80,28 +81,28 @@ func _magentermechanic():
 func _update_all():
 	_update_per_frame()
 	_update_passivenocost()
-	_update_gainclick()
 	_update_automatedpassive()
 	_update_achievsquared()
 	_update_magentermechanic()
 
 func _update_per_frame():
-	%cyyanthings.text = ("you have " + format.number(cyyanthings) + " cyyan things,\ngiving you x" + format.number(cyyanmultiply) + " thing generation")
-	%passivenocost.disabled = !((cyyanthings) >= PASSIVENOCOSTCOST) or passivenocost
+	%cyyanthings.text = ("you have " + format.number(cyyanthings, 0) + " cyyan things,\ngiving you x" + format.number(cyyanmultiply) + " thing generation")
+	%passivenocost.disabled = !(cyyanthings >= PASSIVENOCOSTCOST) or passivenocost
 	
-	%gainclick.disabled = !((cyyanthings) >= GAINCLICKCOST) or gainclick
+	%gainclick.disabled = !(cyyanthings >= GAINCLICKCOST) or gainclick
+	%gainclick.text = "you get 50% of your things\nper click per second" + (("\nthis costs " + format.number(GAINCLICKCOST) + " cyyan things") if !gainclick else ("\n( +" + format.number(%normal.perclickactual/2) + " )"))
 	
-	%automatedpassive.disabled = !((cyyanthings) >= AUTOMATEDPASSIVECOST) or automatedpassive
+	%automatedpassive.disabled = !(cyyanthings >= AUTOMATEDPASSIVECOST) or automatedpassive
 	
-	%timeplayed.disabled = !((cyyanthings) >= TIMEPLAYEDCOST) or timeplayed
+	%timeplayed.disabled = !(cyyanthings >= TIMEPLAYEDCOST) or timeplayed
 	%timeplayed.text = "you get a multiplier on thing\ngeneration based on time played\n( " + ("capped at " if timeplayedboost == 500 else "") + "x" + format.number(timeplayedboost) + " )\n" + (("this costs " + format.number(TIMEPLAYEDCOST) + " cyyan things") if !timeplayed else "")
 	
 	%cyyaninfo.text = "you get 1 cyyan thing every " + format.number(thingspercyyan) + " things you get\nthis menas you get " + format.number(%normal.persecondactual/thingspercyyan) + " cyyan things per second\ncyyan things give you a multiplier to your thing production"
 	
-	%achievsquared.disabled = !((cyyanthings) >= ACHIEVSQUAREDCOST) or achievsquared
+	%achievsquared.disabled = !(cyyanthings >= ACHIEVSQUAREDCOST) or achievsquared
 	
 	%magentermechanic.disabled = !(cyyanthings >= MAGENTERMECHANICCOST) or magentermechanic
-	%magentermechanic.visible = cyyanthings+cyyanthingscosted > MAGENTERMECHANICCOST
+	%magentermechanic.visible = cyyanthingsalltime > MAGENTERMECHANICCOST
 	if !magentermechanic:
 		if cyyanthings >= MAGENTERMECHANICCOST*0.5:
 			%magentermechanic.text = ("MAGENTER THINGS\nthis costs " + format.number(MAGENTERMECHANICCOST) + " cyyan things")
@@ -110,9 +111,6 @@ func _update_per_frame():
 
 func _update_passivenocost():
 	%passivenocost.text = "passive thing generators no\nlonger costs things" + (("\nthis costs " + format.number(PASSIVENOCOSTCOST) + " cyyan things") if !passivenocost else "")
-
-func _update_gainclick():
-	%gainclick.text = "you get 50% of your things\nper click per second" + (("\nthis costs " + format.number(GAINCLICKCOST) + " cyyan things") if !gainclick else ("\n( +" + format.number(%normal.perclickactual/2) + " )"))
 
 func _update_automatedpassive():
 	%automatedpassive.text = "passive thing generators are\nautomatically bought\nevery " + format.number(game.automatortick) + " seconds" + ("\nthis costs " + format.number(AUTOMATEDPASSIVECOST) + " cyyan things" if !automatedpassive else "")
@@ -135,7 +133,6 @@ func save():
 		"nodepath" : self.get_path(),
 		"thingspercyyan" : thingspercyyan,
 		"cyyanthings" : cyyanthings,
-		"cyyanthingscosted" : cyyanthingscosted,
 		#"cyyanmultiply" : cyyanmultiply,
 		"passivenocost" : passivenocost,
 		"gainclick" : gainclick,
--- informateion.gd
+++ informateion.gd
@@ -1,6 +1,7 @@
 extends Control
 @onready var things = get_node("/root/game/tabs/things/normal")
 @onready var cyyan = get_node("/root/game/tabs/things/cyyan")
+@onready var magenter = get_node("/root/game/tabs/things/magenter")
 @onready var format = get_node("/root/game").format
 @onready var game = get_node("/root/game")
 var timestarted = Time.get_unix_time_from_system()
@@ -8,7 +9,8 @@ var timestarted = Time.get_unix_time_from_system()
 
 func _update_per_frame():
 	%informat.text = ("you have made a total of " + format.number(things.thingsalltime) + " things") + (("
-	and a total of " + format.number(cyyan.cyyanthings+cyyan.cyyanthingscosted) + " cyyan things") if (things.cyyanmechanic) else "") + ("
+	and a total of " + format.number(cyyan.cyyanthingsalltime) + " cyyan things") if (things.cyyanmechanic) else "") + (("
+	and a total of " + format.number(magenter.magenterthingsalltime) + " magenter things") if (cyyan.magentermechanic) else "") + ("
 	you have spent " + format.time(int(game.time)) + " on this shitsterpiece") + ("
 	and you started playing " + format.time(int(Time.get_unix_time_from_system()-timestarted)) + " ago")
 
