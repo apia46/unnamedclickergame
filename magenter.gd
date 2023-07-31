e9d0805f9808cb315ac8ecec6a495f1303572452
Beeka <beekayt274@gmail.com>
2023-07-31 21:34:37 +1200

    more progress

--- magenter.gd
+++ magenter.gd
@@ -2,41 +2,63 @@ extends Control
 @onready var format = get_node("/root/game").format
 
 var magenterthings = float(0)
+var magenterthingsalltime = float(0)
 var thingpassivepersecond = float(0)
 var extrathingpassive = float(0)
 var magenteryield = float(0.01)
 var magenterperclick = float(0)
 var magenterperclickactual = float(0)
-const CYYANPERMAGENTER = 1e10
+const CYYANPERMAGENTER = float(1e10)
+var thingspercyyancost = float(1)
 
-func processmagenter(_delta):
+func processmagenter(delta):
 	if !%cyyan.magentermechanic: return
 	magenterperclick = %cyyan.cyyanthings/CYYANPERMAGENTER
 	thingpassivepersecond = pow(magenterthings/10, 0.5)
+	extrathingpassive += thingpassivepersecond*delta
 	
-	magenterperclickactual = (magenterperclick*pow(%normal.timesincelastclick, 0.7))*magenteryield
+	magenterperclickactual = ((magenterperclick)**(%normal.funnyupgrade1boost*1.3))*magenteryield
+	%normal._update_passivething()
 
 func _magenterbutton():
 	magenterthings += magenterperclickactual
+	magenterthingsalltime += magenterperclickactual
 	%normal.timesincelastclick = 0
 	_update_magenterthings()
 
+func _thingspercyyan():
+	magenterthings -= thingspercyyancost
+	thingspercyyancost *= 2
+	%cyyan.thingspercyyan *= 0.9
+	_update_magenterthings()
+	_update_thingspercyyan()
 
 
 func _update_all():
 	_update_magenterthings()
+	_update_thingspercyyan()
 
 func _update_per_frame():
-	%magenterthings3.text = "giving you " + format.number(extrathingpassive) + " extra thing generators total"
+	%magenterthings3.text = "giving you " + format.number(extrathingpassive, 0) + " extra thing generators total"
 	%magenterbutton.text = "Sacrifice your time since last click (" + format.time(%normal.timesincelastclick, format.TIMEMODE.SINGLE) + ")\nat " + format.number(magenteryield*100) + "% yield\nfor " + format.number(magenterperclickactual) + " magenter things"
 
 func _update_magenterthings():
+	processmagenter(0)
 	%magenterthings.text = "you have " + format.number(magenterthings) + " magenter things,"
 	%magenterthings2.text = "giving you " + format.number(thingpassivepersecond) + " thing generators per second,"
-#" + format.number() + "
+	
+	%thingspercyyan.disabled = !(magenterthings >= thingspercyyancost)
 
-func _save():
+func _update_thingspercyyan():
+	%thingspercyyan.text = "decrease things per cyyan\n" + format.number(%cyyan.thingspercyyan) + " -> " + format.number(%cyyan.thingspercyyan*0.9) + "\nthis costs " + format.number(thingspercyyancost) + "\nmagenter things"
+
+
+func save():
 	var save_dict = {
 		"nodepath" = self.get_path(),
+		"magenterthings" = magenterthings,
+		"magenterthingsalltime" = magenterthingsalltime,
+		"extrathingpassive" = extrathingpassive,
+		"thingspercyyancost" = thingspercyyancost,
 	}
 	return save_dict
